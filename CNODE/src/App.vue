8d78a656ac22dd844d137f3d981c6bb9be6685ab
xiaoFeng <804875728@qq.com>
2019-05-22 21:08:13 +0800

    cnode社区搭建

---
+++ CNODE/src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div id="app">
+   
+    <Header></Header>
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components:{
+    Header
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+}
+</style>
---
+++ CNODE/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+   <div class="header">
+       <img src="../assets/cnodejs_light.svg" alt="">
+       <ul>
+           <li><a href="#">首页</a></li>
+           <li><a href="#">新手入门</a></li>
+           <li><a href="#">API</a></li>
+           <li><a href="#">关于</a></li>
+           <li><a href="#">注册</a></li>
+           <li><a href="#">登录</a></li>
+       </ul>
+   </div>
+
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style scoped>
+.header{
+    background-color: #5a5555;
+    
+
+}
+img{
+    max-width:120px;
+    margin-left: 50px;
+    margin-top:10px;
+    padding-bottom: 8px;
+}
+ul>li{
+    list-style: none;
+    display: inline-block;
+    padding-right: 20px;
+    
+    
+    
+}
+ul{
+    float:right;
+    padding:15px 20px;
+}
+a{
+    text-decoration: none;
+    color:azure;
+}
+</style>
